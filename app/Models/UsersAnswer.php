a3be612335a9961b972ce8c4fd37577a3eda443f
sanjie133 <sanjid133@gmail.com>
2017-03-08 00:21:31 +0600

    fix

--- app/Models/UsersAnswer.php
+++ app/Models/UsersAnswer.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class UsersAnswer extends Model
 {
-    //
+    protected $table = 'users_answers';
+    protected $fillable = [
+        'user_id',
+        'question_id',
+        'option_id',
+        'answer',
+        'xtra',
+        'answer_date',
+    ];
+   /*
+    *  $table->unsignedInteger('user_id');
+            $table->unsignedInteger('question_id');
+            $table->unsignedInteger('option_id');
+            $table->unsignedInteger('class_id')->default(0);
+            $table->string('xtra');
+            $table->string('answer');
+            $table->date('answer_date');
+    */
 }
