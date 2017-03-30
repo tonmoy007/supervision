ea0baea9e07e7f9d018aaed8574e227b9724735b
sanjie133 <sanjid133@gmail.com>
2017-03-31 02:07:41 +0600

    science lab and teacher answer added

--- database/seeds/AnswerSeeder.php
+++ database/seeds/AnswerSeeder.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Seeder;
 use App\Models\UsersAnswer;
 use App\Models\User;
 use Carbon\Carbon;
+use App\Models\QuestionType;
 
 class AnswerSeeder extends Seeder
 {
@@ -62,6 +63,73 @@ class AnswerSeeder extends Seeder
                     'option_id' => rand($x, $x+2),
                     'class_id' => $class->id,
                     'answer' => "answer",
+                    'xtra' => 'education',
+                    'answer_date' => Carbon::now()->toDateString()
+                ]);
+                $ans->save();
+                $x+=3;
+            }
+        }
+
+        $types = QuestionType::where('id', '>=', 1)->where('id', '<=', 3)->get();
+        foreach ($types as $type) {
+            $cl = $type->toArray();
+            $x=24;
+
+            $ans = new UsersAnswer([
+                'user_id' => 1,
+                'question_id' => 14,
+                'option_id' => rand(24, 27),
+                'class_id' => $type->id,
+                'answer' => "answer",
+                'xtra' => 'education',
+                'answer_date' => Carbon::now()->toDateString()
+            ]);
+            $ans->save();
+            $ans = new UsersAnswer([
+                'user_id' => 1,
+                'question_id' => 15,
+                'option_id' => rand(28, 30),
+                'class_id' => $type->id,
+                'answer' => "answer",
+                'xtra' => 'education',
+                'answer_date' => Carbon::now()->toDateString()
+            ]);
+            $ans->save();
+            $ans = new UsersAnswer([
+                'user_id' => 1,
+                'question_id' => 16,
+                'option_id' => rand(31, 32),
+                'class_id' => $type->id,
+                'answer' => "answer",
+                'xtra' => 'education',
+                'answer_date' => Carbon::now()->toDateString()
+            ]);
+            $ans->save();
+            $ans = new UsersAnswer([
+                'user_id' => 1,
+                'question_id' => 17,
+                'option_id' => 0,
+                'class_id' => $type->id,
+                'answer' => rand(40, 100),
+                'xtra' => 'education',
+                'answer_date' => Carbon::now()->toDateString()
+            ]);
+            $ans->save();
+        }
+
+        $schools = User::find(1)->schools;
+        $classes = $schools->classes;
+        //$classes = $classes->toArray();
+        foreach ($classes as $class) {
+            $x=12;
+            for ($i = 18; $i <= 21; $i++) {
+                $ans = new UsersAnswer([
+                    'user_id' => 1,
+                    'question_id' => $i,
+                    'option_id' => 0,
+                    'class_id' => $class->id,
+                    'answer' => rand(50, 100),
                     'xtra' => 'education',
                     'answer_date' => Carbon::now()->toDateString()
                 ]);
