8ddb6a015c766c1af6c81183b3a5bedb7317dc26
sanjie133 <sanjid133@gmail.com>
2017-04-08 18:19:04 +0600

    route changesd

--- routes/api.php
+++ routes/api.php
@@ -85,8 +85,8 @@ Route::post('/questions/academic', "Questions\\QuestionController@overallAnswer"
 Route::get('/questions/comment', "Questions\\QuestionController@comment");
 Route::post('/questions/comment', "Questions\\QuestionController@overallAnswer");
 
-Route::get('/reports/general', "Questions\\ReportController@general");
-Route::get('/reports/responsibility', "Questions\\ReportController@responsibility");
-Route::get('/reports/transfer', "Questions\\ReportController@transfer");
-Route::get('/reports/infrastructure', "Questions\\ReportController@infrastructure");
+Route::get('/questions/general', "Questions\\ReportController@general");
+Route::get('/questions/responsibility', "Questions\\ReportController@responsibility");
+Route::get('/questions/transfer', "Questions\\ReportController@transfer");
+Route::get('/questions/infrastructure', "Questions\\ReportController@infrastructure");
 //Route::resource('questions', "Questions\\QuestionController");
