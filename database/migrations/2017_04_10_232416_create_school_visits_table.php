10a01906ea2bedf494cd7f9656ceaa4d250585fe
sanjie133 <sanjid133@gmail.com>
2017-04-10 23:37:17 +0600

    school visit added

---
+++ database/migrations/2017_04_10_232416_create_school_visits_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSchoolVisitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('school_visits', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('visitor_id');
+            $table->unsignedInteger('school_id');
+            $table->date('visit_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('school_visits');
+    }
+}
